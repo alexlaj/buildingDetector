09574fcea0e7b677f749d16f520cc82169708c09
lanin01 <lnorthco@uvic.ca>
2015-03-08 22:41:03 -0700

    Update gaborResponse.m
    Fixed loop

--- gaborResponse.m
+++ gaborResponse.m
@@ -2,7 +2,7 @@ function Ro = gaborResponse(im)
     [n, m] = size(im);
     filtSize = 39;
     R = zeros(n +filtSize-1, m+filtSize-1, 6);
-    for i=1:5
+    for i=1:6
         R(:,:,i) = conv2(im, gaborFilter(i*pi/6, filtSize, filtSize));
     end
     Ro = R(1:n,1:m,:);
