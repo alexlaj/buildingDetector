aecbd3677ffd4230e6d7a39d465b25d131da1c78
Alex LaJeunesse <Alexlajeunesse@gmail.com>
2015-03-29 20:58:40 -0700

    Update colourSeg.m

--- colourSeg.m
+++ colourSeg.m
@@ -1,31 +1,35 @@
 function [segmentedImage] = colourSeg(im)
-    % Returns a matrix of local maxima found in an area with an eucludian
-    % radius of 4.
-    thresh = 0.1;
+    % Based on the local max function created for the gabor filters
+    %
+    % For every pixel, check all surrounding pixels using a mask, then if
+    % there are enough pixels within 10% of the current pixel count it as
+    % part of a segment (set to 1 in the output image).
+    %
+    % This function ignores pixels where the green value is 8% higher than
+    % the green and red values.
+    %
+    % This function is meant to be run twice. If a pixel has been set to
+    % black from the first run we ignore it.
+    
+    % Thresholds (colour threshold and green threshold)
+    cThresh = 0.1;
+    gThresh = 1.08;
+    
     [n, m, ~] = size(im);
+    
+    % Preallocting matricies and setting up the mask
     segmentedImage = zeros([n m]);
-    %mask = [0 0 0 0 1 0 0 0 0;
-    %        0 0 0 1 1 1 0 0 0;
-    %        0 0 1 1 1 1 1 0 0;
-    %        0 1 1 1 1 1 1 1 0;
-    %        1 1 1 1 1 1 1 1 1;
-    %        0 1 1 1 1 1 1 1 0;
-    %        0 0 1 1 1 1 1 0 0;
-    %        0 0 0 1 1 1 0 0 0;
-    %        0 0 0 0 1 0 0 0 0];
     mask = ones([9 9]);
     mask = logical(mask);
     tmpMask = zeros([n m]);
-    % 1s are with a ecludian radius of 4 of the center pixel
     local = zeros([9 9]);
-    % Boundary conditions
     
-    % For each pixel far enough away from the boundary
+    % Go through every pixel
     for x = 1:n
         for y = 1:m
             tmpMask = mask;
-            % Get the 9x9 matrix surrounding it
-            % Get neighborhood from image matrix
+            % Get the 9x9 matrix surrounding the current pixel. Have to
+            % adjust the mask for the pixels near the edge of the image.
             sX = max(1,x-4);
             eX = min(9+sX,n);
             sY = max(1,y-4);
@@ -69,20 +73,33 @@ function [segmentedImage] = colourSeg(im)
             green = im(x,y,2);
             blue = im(x,y,3);
             cnt = 0;
-            for xx = 1:nn
-                if (localR(xx)<(red+thresh*red)) && (localR(xx)>(red-thresh*red)) && (localG(xx)<(green+thresh*green)) && (localG(xx)>(green-thresh*green)) && (localB(xx)<(blue+thresh*blue)) && (localB(xx)>(blue-thresh*blue))
-                    cnt = cnt+1;
-                else
-                    cnt = cnt-1;
-                end
-            end
-            if cnt>3
-                segmentedImage(x,y) = 1;
+            % Try to remove green
+            % Checks if G is a percentage higher than R and B
+            if im(x,y,2) > im(x,y,1)*gThresh && im(x,y,2) > im(x,y,3)*gThresh 
+                segmentedImage(x,y) = 0; 
+            % If pixel is completely black do not count as a segment 
+            % This is mostly for the second run through 
+            elseif im(x,y,1)==0 && im(x,y,2)==0 && im(x,y,3)==0 
+                segmentedImage(x,y) = 0; 
             else
-                segmentedImage(x,y) = 0;
+                % Count pixels of similar colour in the neighborhood.
+                % Decrement the counter if a pixel is not a similar colour,
+                % increment if it is a similar colour.
+                for xx = 1:nn
+                    if (localR(xx)<(red+cThresh*red)) && (localR(xx)>(red-cThresh*red)) && (localG(xx)<(green+cThresh*green)) && (localG(xx)>(green-cThresh*green)) && (localB(xx)<(blue+cThresh*blue)) && (localB(xx)>(blue-cThresh*blue))
+                        cnt = cnt+1;
+                    else
+                        cnt = cnt-1;
+                    end
+                end
+                % If there are enough pixels of a similar colour near,
+                % set the pixel to 1, otherwise 0.
+                if cnt>2
+                    segmentedImage(x,y) = 1;
+                else
+                    segmentedImage(x,y) = 0;
+                end
             end
         end
     end
-    
-
     
